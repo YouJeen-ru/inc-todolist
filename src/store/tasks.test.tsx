3b31285248a0a5573e57ca335b6ec855b1db29b6
YouJeen-ru <1.rouch@mail.ru>
2021-10-25 11:19:47 +0300

    training tdd

--- src/store/tasks.test.tsx
+++ src/store/tasks.test.tsx
@@ -1,27 +1,44 @@
-import {ActionType, salaryReducer} from "./tasks";
+import {getBanknoteList, getSum, getTriangleType, isEvenIndexSumGreater, isSquareGreater, sum} from "./tasks";
 
 
-test('Next salary (sum)', () => {
-    const salary = 500
-    const actionSum: ActionType = {type: 'sum', number: 100}
-
-    expect(salaryReducer(salary, actionSum )).toBe(600)
+test('reduce numbers', () => {
+    expect(sum(1,2,3,4,5)).toBe(15)
+    expect(sum(1,1,1,6,1)).toBe(10)
 })
 
-test('Next salary (mult)', () => {
-    const salary = 500
-
-    expect(salaryReducer(salary, {type: 'mult', number: 2})).toBe(1000)
+test("get Triangle Type", ()=> {
+    expect(getTriangleType(1, 1, 1)).toBe("10")
+    expect(getTriangleType(3, 3, 2)).toBe("01")
+    expect(getTriangleType(5, 4, 3)).toBe("11")
+    expect(getTriangleType(10, 5, 5)).toBe("00")
 })
 
-test('Next salary (div)', () => {
-    expect(salaryReducer(1500, {type: 'div', number: 3})).toBe(500)
+test("get Sum", ()=> {
+    expect(getSum(1000)).toBe(1)
+    expect(getSum(0)).toBe(0)
+    expect(getSum(1234)).toBe(10)
+    expect(getSum(9999)).toBe(36)
 })
 
-test('Next salary (sub)', () => {
-    const salary = 1500
-    const result = salaryReducer(salary, {type: 'sub', number: 200})
-
-    expect(result).toBe(1300)
+test("is Even Sum Greater", ()=> {
+    expect(isEvenIndexSumGreater([1, 100, 2, 200])).toBe(false)
+    expect(isEvenIndexSumGreater([100, 1, 200, 2])).toBe(true)
 })
 
+
+test("is Square Greater Than Circle", ()=> {
+    const sCr = 3.14
+    const sSq = 4
+    const result = isSquareGreater(sCr, sSq)
+    expect(result).toBe(true)
+})
+test("get banknote list", ()=> {
+    const result1500 = getBanknoteList(1500)
+    const result23 = getBanknoteList(23)
+    expect(result1500[0]).toBe(1000)
+    expect(result1500[1]).toBe(500)
+    expect(result23[0]).toBe(20)
+    expect(result23[1]).toBe(2)
+    expect(result23[2]).toBe(1)
+
+})
--- src/store/tasks.tsx
+++ src/store/tasks.tsx
@@ -1,25 +1,101 @@
-export type ActionType = {
-    type: 'sum' | 'mult' | 'div' | 'sub'
-    number?: number
+// 1. Функция принимает параметром целые положительные числа и возвращает
+// их сумму.
+
+export const sum = (...numbers:number[]): number => {
+
+    return numbers.reduce((acc, el) => acc + el)
+}
+// 2. Функция getTriangleType принимает три параметра:
+// длины сторон треугольника.
+// Функция должна возвращать:
+//  - "01", если треугольник равнобедренный,
+//  - "10", если треугольник равносторонний,
+//  - "11", если треугольник обычный,
+//  - "00", если такого треугольника не существует.
+
+
+export const getTriangleType = (a:number,b:number,c:number): string => {
+    if(a  + b > c && b + c > a && c + a > b) {
+        if(a === b || b === c || c === a) {
+            return '10'
+        } else if (a === b && b === c ) {
+            return '01'
+        }
+        else {
+            return '11'
+        }
+    } else {
+        return '00'
+    }
 }
 
-export function salaryReducer(salary: number, action: ActionType) {
-    switch (action.type) {
-        case 'sum': {
-            return salary + action.number!
-        }
-        case "mult": {
-            return salary * action.number!
-        }
-        case "div": {
-            return salary / action.number!
-        }
-        case "sub": {
-            return salary - action.number!
-        }
-        default: {
-            return salary
+// 3. Функция getSum принимает параметром целое число и возвращает
+// сумму цифр этого числа
+
+export function getSum(i: number): number {
+    if (i === 0) return i
+
+
+    const sum = i.toString().split('').map(Number)
+    return sum.reduce((acc, el) => {
+        return acc + el
+    })
+}
+
+
+// 4. Функция принимает isEvenIndexSumGreater параметром массив чисел.
+// Если сумма чисел с чётными ИНДЕКСАМИ!!! (0 как чётный индекс) больше
+// суммы чисел с нечётными ИНДЕКСАМИ!!!, то функция возвращает true.
+// В противном случае - false.
+
+export const isEvenIndexSumGreater = (arrOfNumbers: Array<number>): boolean => {
+
+    let even = 0
+    let odd = 0
+    for (let i = 0 ; i < arrOfNumbers.length;i++) {
+        if (i % 2 === 0) {
+            even += arrOfNumbers[i]
+        } else {
+            odd += arrOfNumbers[i]
         }
     }
+    return even > odd
+}
+
+
+// 5. Функция isSquareGreater принимает два параметра: площадь круга и
+// площадь квадрата. Функция должна возвращать true если круг не будет выступать за пределы
+// квадрата и false в противном случае. Центры фигур совпадают.
+
+export function isSquareGreater(circle: number, square: number): boolean {
+    let circleArea = Math.PI * (circle*2)
+    let squareArea = square ** 2
+    return circleArea >= squareArea
+
 
 }
+
+
+// 6. Функция-банкомат принимает параметром целое натуральное число (сумму).
+// Возвращает массив с наименьшим количеством купюр, которыми можно выдать эту
+// сумму. Доступны банкноты следующих номиналов:
+// const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1].
+// Считаем, что количество банкнот каждого номинала не ограничено
+
+export const getBanknoteList = (x:number) => {
+    const banknotes = [1000, 500, 100, 50, 20, 10, 5, 2, 1]
+    const resultBanknots:number[] = []
+    if(x > 0) {
+        for (let i = 0; i < banknotes.length; i++) {
+            let note = banknotes[i]
+            while (x - note >= 0 ) {
+                x -= note
+                resultBanknots.push(note)
+            }
+        }
+    } else {
+        console.log('')
+    }
+    return resultBanknots
+}
+
